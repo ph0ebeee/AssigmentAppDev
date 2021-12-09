2e6c0badb2962cc7324e7f329128829d51fe1f2c
phoebe <210527H@mymail.nyp.edu.sg>
2021-12-10 01:20:28 +0800

    phoebe

--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+#from templates.Forms import CreateUserForm,CreateCustomerForm
+
 
 app = Flask(__name__)
 
@@ -6,6 +8,27 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('home.html')
+
+
+
+#@app.route('/contactUs', methods=['GET', 'POST'])
+#def feedback():
+ #   feedback = CreateUserForm(request.form)
+  #  if request.method == 'POST' and feedback.validate():
+   #     users_dict = {}
+    #    db = shelve.open('user.db', 'c')
+
+     #   try:
+      #      users_dict = db['Users']
+       # except:
+        #    print("Error in retrieving Users from user.db.")
+
+        #user = User.User(feedback.name.data, feedback.response.data)
+        #users_dict[user.get_user_id()] = user
+        #db['Users'] = users_dict
+
+
+
 
 
 if __name__ == '__main__':
