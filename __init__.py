9a03a5587183e0472626747db0ae6f91115d904c
phoebe <210527H@mymail.nyp.edu.sg>
2021-12-23 01:25:41 +0800

    phoebe

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
+import paypalrestsdk
 #from templates.Forms import CreateUserForm,CreateCustomerForm
 
 
@@ -9,6 +10,59 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+
+paypalrestsdk.configure({
+  "mode": "sandbox", # sandbox or live
+  "client_id": "AfRIwzrYKNDDjzhwa6wx4MAuoKf-7j0t76lAYyH-OEAC_XwtpxZmWX_VQ7M4INH10LUrIsESHWDFcUmm",
+  "client_secret": "ELm9nAnFFJV46yrWD8GqCwqSViSw8wda7DGGBJnxmMe2v7mCVDHp88HwLRzhj_3ehT73-ZdUx2jfOr_O" })
+
+@app.route('/checkout')
+def index():
+    return render_template('paypal.html')
+
+@app.route('/payment', methods=['POST'])
+def payment():
+
+    payment = paypalrestsdk.Payment({
+        "intent": "sale",
+        "payer": {
+            "payment_method": "paypal"},
+        "redirect_urls": {
+            "return_url": "http://localhost:3000/payment/execute",
+            "cancel_url": "http://localhost:3000/"},
+        "transactions": [{
+            "item_list": {
+                "items": [{
+                    "name": "testitem",
+                    "sku": "12345",
+                    "price": "500.00",
+                    "currency": "USD",
+                    "quantity": 1}]},
+            "amount": {
+                "total": "500.00",
+                "currency": "USD"},
+            "description": "This is the payment transaction description."}]})
+
+    if payment.create():
+        print('Payment success!')
+    else:
+        print(payment.error)
+
+    return jsonify({'paymentID' : payment.id})
+
+@app.route('/execute', methods=['POST'])
+def execute():
+    success = False
+
+    payment = paypalrestsdk.Payment.find(request.form['paymentID'])
+
+    if payment.execute({'payer_id' : request.form['payerID']}):
+        print('Execute success!')
+        success = True
+    else:
+        print(payment.error)
+
+    return jsonify({'success' : success})
 
 
 #@app.route('/contactUs', methods=['GET', 'POST'])
