db3469f86d43f6d2ee9933cae167be955eb04298
anna <zanna.oop@gmail.com>
2022-01-17 21:45:09 +0800

    retrieve not working

--- __init__.py
+++ __init__.py
@@ -33,7 +33,7 @@ def home():
     image3 = './static/Assets/images/imageCarousel_3.jpg' 
     image4 = './static/Assets/images/imageCarousel_4.jpg' 
     image5 = './static/Assets/images/imageCarousel_5.jpg' 
-    return render_template('home.html',image1=image1,image2=image2,image3=image3,image4=image4,image5=image5)
+    return render_template('staff.html',image1=image1,image2=image2,image3=image3,image4=image4,image5=image5)
 
 #route for login form to be seen on loginPage.html  - viona
 @app.route('/Login', methods=['GET', 'POST'])
@@ -195,6 +195,9 @@ def staffaccount():
         #use JS to change the layout of the navbar according Staff account
     return render_template('staff/staff_account.html', form=UpdateStaff)
 
+@app.route('/MainPage')
+def MainPage():
+    return render_template('staff.html')
 
 @app.route('/createCustomers', methods=['GET', 'POST'])
 def create_customers():
