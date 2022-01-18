a5bf900ee6ffc6d21894890493fd1817a44bc0b7
phoebe <210527H@mymail.nyp.edu.sg>
2022-01-18 22:12:13 +0800

    Phoebe OOP

--- forms/forms.py
+++ forms/forms.py
@@ -1,4 +1,4 @@
-from wtforms import Form, StringField, SelectField, TextAreaField, PasswordField, validators, RadioField
+from wtforms import Form, StringField, SelectField, TextAreaField, PasswordField, validators, RadioField, IntegerField
 from wtforms.fields import EmailField, DateField
 
 class signupForm(Form):
@@ -29,3 +29,10 @@ class CreateStaffForm(Form):
     address = TextAreaField('Mailing Address:', [validators.length(max=200), validators.DataRequired()])
     role = RadioField('Role', choices=[('F', 'Fellow'), ('S', 'Senior'), ('P', 'Professional')], default='F')
     remarks = TextAreaField('Remarks', [validators.Optional()])
+
+class CreditCardForm(Form):
+    name = StringField('Name:', [validators.Length(min=1, max=150), validators.DataRequired()])
+    address = TextAreaField('Mailing Address:', [validators.length(max=200), validators.DataRequired()])
+    card_no = IntegerField('Card Number:', [validators.Length(min=16, max=16), validators.DataRequired()])
+    expiry = DateField('Valid Till:', [validators.Length(min=1, max=150), validators.DataRequired()])
+    cvc = IntegerField('CVC:', [validators.Length(min=3, max=3), validators.DataRequired()])
